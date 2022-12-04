c38e8e776c3966d32e59b37bba2c2248a857bf6a
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2022-12-04 17:55:18 +0100

    temp landscape interaction logic

--- src/js/idealisedLandscapes.js
+++ src/js/idealisedLandscapes.js
@@ -2,37 +2,62 @@ import * as p5 from "p5";
 
 //a P5 moire pattern.
 let s = (sk) => {
-  let layers = [];
+  let privacyStatementP5;
+  let privacyStatementJS;
+  let privacyStatementBoundingClientRect;
+  let privacyStatementBoundingClientRectBottom;
+  let difference;
+  let posY;
 
-  // sk.translate(window.innerWidth/2,window.innerHeight/2);
   sk.setup = () => {
-    let gfx = sk.createGraphics(window.innerWidth, window.innerHeight);
-    let gfx2;
-
     sk.createCanvas(window.innerWidth, window.innerHeight).parent(
       "idealised-landscapes"
     );
-    sk.angleMode(sk.DEGREES);
-    sk.imageMode(sk.CENTER);
-    sk.translate(window.innerWidth / 2, window.innerHeight / 2);
-    sk.background(40);
-    gfx.stroke(200);
-    gfx.strokeWeight(3);
-    gfx.line(0, 0, window.innerWidth, 0);
-    for (let i = 0; i < 1000; i++) {
-      gfx.point(
-        Math.random() * window.innerWidth,
-        Math.random() * window.innerHeight
-      );
-    }
 
-    gfx2 = { ...gfx };
-    sk.image(gfx, 0, 0);
-    sk.rotate(1);
-    sk.image(gfx2, 0, 0);
+    // styling
+    sk.noFill();
+    sk.rectMode(sk.CENTER);
+    sk.strokeWeight(1);
+    sk.stroke(255);
+
+    sk.getDivAndCanvas();
   };
 
-  sk.draw = () => {};
+  sk.draw = () => {
+    sk.clear();
+    sk.line(0, 0, sk.width, sk.height);
+    sk.line(sk.width, 0, 0, sk.height);
+
+    privacyStatementP5 = sk.select(".privacyStatement");
+
+    // div animation
+    if (sk.width > sk.height) {
+      // horizontal viewport
+      posY = difference * Math.pow(sk.sin((sk.frameCount / sk.width) * 4), 2);
+    } else {
+      // vertical viewport
+      posY = difference * Math.pow(sk.sin(sk.frameCount / sk.height / 2), 2);
+    }
+    privacyStatementP5.position(0, -posY);
+  };
+
+  sk.windowResized = () => {
+    resizeCanvas(sk.windowWidth, sk.windowHeight);
+
+    // reset the position of #privacyStatement
+    privacyStatementP5.position(0, 0);
+
+    sk.getDivAndCanvas();
+  };
+
+  sk.getDivAndCanvas = () => {
+    privacyStatementJS = document.getElementById("privacyStatement");
+    privacyStatementBoundingClientRect =
+      privacyStatementJS.getBoundingClientRect();
+    privacyStatementBoundingClientRectBottom =
+      privacyStatementBoundingClientRect.bottom;
+    difference = privacyStatementBoundingClientRectBottom - sk.height;
+  };
 };
 
 const P5 = new p5(s);
