ea5e94b06c0ae38ce9467a2e07415dfc5bc06b12
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2022-12-09 19:17:42 +0100

    onboarding setup

--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,36 @@ import * as bootstrap from "bootstrap";
 import DoSlide from "do-slide";
 
 const slide = new DoSlide(".ds-container", {
-  horizontal: false,
+  duration: 500,
+  timingFunction: "cubic-bezier(0.8, 0, 0.2, 1.0)",
+  minInterval: 50,
+});
+
+const navbar = document.querySelector(".navbar");
+const navbarSupportedContent = document.getElementById(
+  "navbar-supported-content"
+);
+
+// rip
+// if (navbarSupportedContent.classList.contains("collapsing")) {
+//   console.log("cagotto molle");
+//   navbar.classList.add("navbar-border");
+// } else if (
+//   navbarSupportedContent.classList.contains("collapse") &&
+//   navbarSupportedContent.classList.contains("show")
+// ) {
+//   console.log("caca");
+//   navbar.classList.add("navbar-border");
+// } else {
+//   navbar.classList.remove("navbar-border");
+// }
+
+// ok
+navbarSupportedContent.addEventListener("shown.bs.collapse", function () {
+  navbar.classList.add("navbar-border");
+});
+navbarSupportedContent.addEventListener("hidden.bs.collapse", function () {
+  if (window.scrollY == 0) {
+    navbar.classList.remove("navbar-border");
+  }
 });
