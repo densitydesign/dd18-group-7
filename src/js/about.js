b4da6d127161b5a31e1ca6661864c41e62a5832c
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2022-12-11 18:23:37 +0100

    about page initial structure

--- src/js/about.js
+++ src/js/about.js
@@ -3,9 +3,17 @@ import "./main";
 
 const navbar = document.querySelector(".navbar");
 const main = document.querySelector("main");
+const headingAll = document.querySelectorAll(".t-sticky-1");
 
 // Responsive content layout
 main.style.marginTop = navbar.offsetHeight + "px";
+headingAll.forEach((heading) => {
+  heading.style.top = navbar.offsetHeight + "px";
+});
+
 window.addEventListener("resize", (event) => {
   main.style.marginTop = navbar.offsetHeight + "px";
+  headingAll.forEach((heading) => {
+    heading.style.top = navbar.offsetHeight + "px";
+  });
 });
