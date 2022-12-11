47e06a52c74efe9e108fdc14d6132114e5861051
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2022-12-11 18:38:24 +0100

    index alignment

--- src/js/about.js
+++ src/js/about.js
@@ -4,16 +4,20 @@ import "./main";
 const navbar = document.querySelector(".navbar");
 const main = document.querySelector("main");
 const headingAll = document.querySelectorAll(".t-sticky-1");
+const indexLine = document.getElementById("index-line");
+const indexAligner = document.getElementById("index-aligner");
 
 // Responsive content layout
 main.style.marginTop = navbar.offsetHeight + "px";
 headingAll.forEach((heading) => {
   heading.style.top = navbar.offsetHeight + "px";
 });
+indexLine.style.height = indexAligner.offsetHeight + "px";
 
 window.addEventListener("resize", (event) => {
   main.style.marginTop = navbar.offsetHeight + "px";
   headingAll.forEach((heading) => {
     heading.style.top = navbar.offsetHeight + "px";
   });
+  indexLine.style.height = indexAligner.offsetHeight + "px";
 });
