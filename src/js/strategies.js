7a841d65d37a1130697dad4d0dcaf2a7e08415aa
benedettoandrea <andrea1.benedetto@mail.polimi.it>
2022-12-14 19:07:47 +0100

    mouseover setup + new video

--- src/js/strategies.js
+++ src/js/strategies.js
@@ -19,3 +19,21 @@ navbarSupportedContent.addEventListener("show.bs.collapse", (event) => {
 navbarSupportedContent.addEventListener("hidden.bs.collapse", (event) => {
   navbar.classList.remove("navbar-border");
 });
+
+// Mouseover
+let regulatoryStatementsContentCopyU1 = document.getElementById(
+  "regulatory-statements-content-copy-u1"
+);
+let regulatoryStatementsContentCopyU1Image = document.getElementById(
+  "regulatory-statements-content-copy-u1-image"
+);
+
+regulatoryStatementsContentCopyU1.addEventListener("mouseover", (event) => {
+  regulatoryStatementsContentCopyU1Image.classList.remove("opacity-0");
+  regulatoryStatementsContentCopyU1Image.classList.add("opacity-100");
+});
+
+regulatoryStatementsContentCopyU1.addEventListener("mouseleave", (event) => {
+  regulatoryStatementsContentCopyU1Image.classList.remove("opacity-100");
+  regulatoryStatementsContentCopyU1Image.classList.add("opacity-0");
+});
