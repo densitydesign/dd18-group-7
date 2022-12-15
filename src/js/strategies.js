7c97bcd5b86cdfbce279c53157dcb314ab3d5e81
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2022-12-15 02:45:55 +0100

    cringe mouseover js + thumbnails
    Ghosst

--- src/js/strategies.js
+++ src/js/strategies.js
@@ -20,20 +20,90 @@ navbarSupportedContent.addEventListener("hidden.bs.collapse", (event) => {
   navbar.classList.remove("navbar-border");
 });
 
-// Mouseover
+// Regulatory statements
 let regulatoryStatementsContentCopyU1 = document.getElementById(
   "regulatory-statements-content-copy-u1"
 );
 let regulatoryStatementsContentCopyU1Image = document.getElementById(
   "regulatory-statements-content-copy-u1-image"
 );
-
 regulatoryStatementsContentCopyU1.addEventListener("mouseover", (event) => {
   regulatoryStatementsContentCopyU1Image.classList.remove("opacity-0");
   regulatoryStatementsContentCopyU1Image.classList.add("opacity-100");
 });
-
 regulatoryStatementsContentCopyU1.addEventListener("mouseleave", (event) => {
   regulatoryStatementsContentCopyU1Image.classList.remove("opacity-100");
   regulatoryStatementsContentCopyU1Image.classList.add("opacity-0");
+});
+
+let regulatoryStatementsContentCopyW1 = document.getElementById(
+  "regulatory-statements-content-copy-w1"
+);
+regulatoryStatementsContentCopyW1.addEventListener("mouseover", (event) => {
+  regulatoryStatementsContentCopyW1.classList.remove("opacity-0");
+  regulatoryStatementsContentCopyW1.classList.add("opacity-100");
+});
+regulatoryStatementsContentCopyW1.addEventListener("mouseleave", (event) => {
+  regulatoryStatementsContentCopyW1.classList.remove("opacity-100");
+  regulatoryStatementsContentCopyW1.classList.add("opacity-0");
+});
+
+let regulatoryStatementsContentCopyW2 = document.getElementById(
+  "regulatory-statements-content-copy-w2"
+);
+regulatoryStatementsContentCopyW2.addEventListener("mouseover", (event) => {
+  regulatoryStatementsContentCopyW2.classList.remove("opacity-0");
+  regulatoryStatementsContentCopyW2.classList.add("opacity-100");
+});
+regulatoryStatementsContentCopyW2.addEventListener("mouseleave", (event) => {
+  regulatoryStatementsContentCopyW2.classList.remove("opacity-100");
+  regulatoryStatementsContentCopyW2.classList.add("opacity-0");
+});
+
+let regulatoryStatementsContentCopyW3 = document.getElementById(
+  "regulatory-statements-content-copy-w3"
+);
+regulatoryStatementsContentCopyW3.addEventListener("mouseover", (event) => {
+  regulatoryStatementsContentCopyW3.classList.remove("opacity-0");
+  regulatoryStatementsContentCopyW3.classList.add("opacity-100");
+});
+regulatoryStatementsContentCopyW3.addEventListener("mouseleave", (event) => {
+  regulatoryStatementsContentCopyW3.classList.remove("opacity-100");
+  regulatoryStatementsContentCopyW3.classList.add("opacity-0");
+});
+
+let regulatoryStatementsContentCopyW4 = document.getElementById(
+  "regulatory-statements-content-copy-w4"
+);
+regulatoryStatementsContentCopyW4.addEventListener("mouseover", (event) => {
+  regulatoryStatementsContentCopyW4.classList.remove("opacity-0");
+  regulatoryStatementsContentCopyW4.classList.add("opacity-100");
+});
+regulatoryStatementsContentCopyW4.addEventListener("mouseleave", (event) => {
+  regulatoryStatementsContentCopyW4.classList.remove("opacity-100");
+  regulatoryStatementsContentCopyW4.classList.add("opacity-0");
+});
+
+let regulatoryStatementsContentCopyW5 = document.getElementById(
+  "regulatory-statements-content-copy-w5"
+);
+regulatoryStatementsContentCopyW5.addEventListener("mouseover", (event) => {
+  regulatoryStatementsContentCopyW5.classList.remove("opacity-0");
+  regulatoryStatementsContentCopyW5.classList.add("opacity-100");
+});
+regulatoryStatementsContentCopyW5.addEventListener("mouseleave", (event) => {
+  regulatoryStatementsContentCopyW5.classList.remove("opacity-100");
+  regulatoryStatementsContentCopyW5.classList.add("opacity-0");
+});
+
+let regulatoryStatementsContentCopyW6 = document.getElementById(
+  "regulatory-statements-content-copy-w6"
+);
+regulatoryStatementsContentCopyW6.addEventListener("mouseover", (event) => {
+  regulatoryStatementsContentCopyW6.classList.remove("opacity-0");
+  regulatoryStatementsContentCopyW6.classList.add("opacity-100");
+});
+regulatoryStatementsContentCopyW6.addEventListener("mouseleave", (event) => {
+  regulatoryStatementsContentCopyW6.classList.remove("opacity-100");
+  regulatoryStatementsContentCopyW6.classList.add("opacity-0");
 });
