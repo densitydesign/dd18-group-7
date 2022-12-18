79dd33aa2729fd3392322b0f1061cdab086b531c
Andrea Benedetto <34688096+benedettoandrea@users.noreply.github.com>
2022-12-18 20:39:04 +0100

    polishing
    Empty the Bones of You

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     index: "./src/js/index.js",
     strategies: "./src/js/strategies.js",
